3ed597adfc0d3ef86f7e88cef324c5d9016fdcc3
Jeff Eng <jeff@jeffeng.me>
2016-06-30 16:37:57 -0700

    added random number generator and control flow for the various scenarios.

--- Fingers Guessing Game/Fingers Guessing Game/ViewController.swift
+++ Fingers Guessing Game/Fingers Guessing Game/ViewController.swift
@@ -15,7 +15,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var feedbackLabel: UILabel!
     
     @IBAction func submitGuessButtonClicked(sender: AnyObject) {
-    
+        let randomNumber = String(arc4random_uniform(6))
+        
+        if randomNumber == guessTextField.text {
+            feedbackLabel.text = "You correctly guessed \(randomNumber)."
+        } else {
+            feedbackLabel.text = "You guessed wrong. It was \(randomNumber)."
+        }
     }
     
     override func viewDidLoad() {
